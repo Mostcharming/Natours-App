47e6b509e612370b66f110003815103275094681
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-10-10 13:23:37 +0100

    Initial Commit

---
+++ public/js/stripe.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+
+export const bookTour = async tourId => {
+  try {
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Natours Application
+
+Built using modern technologies: node.js, express, mongoDB, mongoose
+
+By Mayowa F Fredrick, Tutored by Jadon Schmedtmann
